489e69a11e3aec400130e9636bfc1959a68e06eb
vaalkor <groggydoggie@protonmail.com>
2021-10-03 10:52:32 +0100

    Added filtering stuff. GetRandom not implemented yet

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 'use strict';
 var _scrapeData = [];
+var _filteredMaps = [];
 var _staticHtml = undefined;
+var _labels = {};
+var _sortOrder = -1;
+var _sortBy = undefined;
 
 function sort(a,b)
 {
-    return (a.RobRating||0)>(b.RobRating||0)?-1:1;
+    if(_sortBy == null) return a;
+    return (a[_sortBy]||0) > (b[_sortBy]||0) ? -1*_sortOrder : 1*_sortOrder;
+}
+
+function mapFilter(map, nameFilter, minRating){
+    if(nameFilter && !(map.Name.toLowerCase().includes(nameFilter.toLowerCase()))) return false;
+    if(minRating >= 0 && (map.RobRating === undefined || map.RobRating === null)) return false;
+    if(minRating >= 0 && map.RobRating < minRating) return false;
+    return true;
 }
 
 function filterMaps(){
-   return _scrapeData.slice();
+    _filteredMaps = _scrapeData.slice();
+    
+    if(!_staticHtml) return;
+    _sortOrder = document.querySelector('#sortAscending').checked ? -1 : 1;
+    _sortBy = [...document.querySelectorAll('#sortBy option')].find(x=>x.selected).value;
+    let nameFilter = document.querySelector('#nameFilter').value;
+    let minRating = document.querySelector('#ratingSlider').value;
+    _filteredMaps = _filteredMaps.filter(x => mapFilter(x, nameFilter, minRating));
+    _filteredMaps.sort(sort);
+}
+
+function handleRatingSlider(e){
+    let slider = document.querySelector('#ratingSlider');
+    let minRating = slider.value < 0 ? 'None selected' : slider.value;
+    document.querySelector('#ratingSliderText').innerText = `Minimum Rating: ${minRating}`
+    m.redraw();
 }
 
 function getLabels(map){
     if(map.RobLabels === null || map.RobLabels === undefined || map.RobLabels.length === 0) return [];
     return map.RobLabels.map(x => m("div", {"class":"map-label"}, m("span", x)));
+}
+
+function resetFilter(e){
+    document.querySelector('#nameFilter').value = '';
+    document.querySelector('#sortBy option').selected = true
+    document.querySelector('#ratingSlider').value = -0.5;
+    handleRatingSlider();
+    m.redraw();
+}
+
+function getRandomMap(e){
+    // alert('bollocks!');
 }
 
 function makeRow(map) {
@@ -28,8 +67,7 @@ function makeRow(map) {
 
 var Table = {
     view: function(){
-        let filteredMaps = filterMaps();
-        filteredMaps.sort(sort);
+        filterMaps();
 
         return m("div", {"class":"container"}, 
             m("table", {"class":"table table-striped","id":"fixed-table-header"}, [
@@ -42,16 +80,25 @@ var Table = {
                         m("th", {"scope":"col"}, "Labels")
                     ]
                     )),
-                m("tbody", filteredMaps.map(x => makeRow(x)))
+                m("tbody", _filteredMaps.map(x => makeRow(x)))
             ])
         );
+    }
+}
+
+var StaticHtml = {
+    view: function(){
+        return m.trust(_staticHtml);
+    },
+    oncreate: function(){
+        initialiseStatic();
     }
 }
 
 var App = {
     view: function(){
         let pieces = [];
-        if(_staticHtml) pieces.push(m.trust(_staticHtml));
+        if(_staticHtml) pieces.push(m(StaticHtml));
         if(_scrapeData) pieces.push(m(Table))
         return pieces;
     }
@@ -62,8 +109,18 @@ m.mount(document.body, App);
 fetch('index-static.html').then(x=>x.text()).then(x => {
     _staticHtml = x;
     m.redraw();
-}); 
+});
 
 m.request({method: 'GET',url: 'scrape_data.json'}).then((x) => {
     _scrapeData = x.MapInfo;
 });
+
+
+function initialiseStatic(){
+    document.querySelector('#ratingSlider').addEventListener("input", handleRatingSlider);
+
+    ['#sortAscending', '#sortDescending', '#sortBy', '#nameFilter'].forEach(x => document.querySelector(x).addEventListener("input", () => m.redraw()));
+
+    document.querySelector('#getRandomMapButton').addEventListener("click", getRandomMap);
+    document.querySelector('#resetFilterButton').addEventListener("click", resetFilter);
+}
