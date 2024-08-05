11fc64858d171fcf8c86477db93f63df243abae9
vaalkor <groggydoggie@protonmail.com>
2024-08-05 11:43:06 +0100

    Formatted index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 var _sortByProperties = [
     { propertyName: 'RobRating', friendlyName: 'Rating' },
-    { propertyName: 'InitialRatingTimestamp', friendlyName: 'First Rated'}
+    { propertyName: 'InitialRatingTimestamp', friendlyName: 'First Rated' }
 ];
 var _storage = {
     sortBy: _sortByProperties[0].propertyName,
@@ -12,18 +12,18 @@ var _storage = {
     minRating: 0,
     includeLabels: [],
     excludeLabels: [],
-    loadFromLocalStorage: function(){
+    loadFromLocalStorage: function () {
         const savedData = localStorage.getItem('storage');
         if (savedData) {
             Object.assign(this, JSON.parse(savedData));
         }
     },
-    save: function(){
+    save: function () {
         localStorage.setItem('storage', JSON.stringify(this));
     }
 }
 
-var _scrapeData = {MapInfo: [], MapRatingGraphData: []};
+var _scrapeData = { MapInfo: [], MapRatingGraphData: [] };
 var _foundLabels = {};
 var _defaultLabel = 'label-blue';
 var _labelColourMap = {
@@ -38,23 +38,23 @@ var _labelColourMap = {
     'Meme': 'label-purple'
 }
 var _foundLabels = [];
-var _dayPrettyPrint = {0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'}
-var _monthPrettyPrint = {0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec'}
+var _dayPrettyPrint = { 0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat' }
+var _monthPrettyPrint = { 0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec' }
 
-var $   = (query) => document.querySelector(query);
-var $$  = (query) => document.querySelectorAll(query);
-var saveElemValue   = (query) => window.localStorage.setItem(query, $(query).value);
-var saveElemValues  = (...queries) => queries.forEach(x => saveElemValue(x));
+var $ = (query) => document.querySelector(query);
+var $$ = (query) => document.querySelectorAll(query);
+var saveElemValue = (query) => window.localStorage.setItem(query, $(query).value);
+var saveElemValues = (...queries) => queries.forEach(x => saveElemValue(x));
 var saveJsonValue = (key, value) => window.localStorage.setItem(key, JSON.stringify(value));
 var getAndSetElemValues = (...keys) => keys.forEach(x => getAndSetElemValue(x));
 var getLabelColour = label => (label in _labelColourMap) ? _labelColourMap[label] : _defaultLabel;
 
-function getAndSetElemValue(key){
+function getAndSetElemValue(key) {
     let val = window.localStorage.getItem(key);
-    if(val !== null && val !== undefined) $(key).value = val;
+    if (val !== null && val !== undefined) $(key).value = val;
 }
 
-function redraw(){
+function redraw() {
     m.redraw();
 }
 
@@ -69,7 +69,7 @@ function mapFilter(map, nameFilter, minRating) {
     if (minRating >= 0 && (map.RobRating === undefined || map.RobRating === null)) return false;
     if (minRating >= 0 && map.RobRating < minRating) return false;
     if (map.RobLabels == null && _storage.includeLabels.length !== 0) return false;
-    if (map.RobLabels && map.RobLabels.reduce((a,b)=> _storage.includeLabels.includes(b) ? a+1: a, 0) !== _storage.includeLabels.length) return false;
+    if (map.RobLabels && map.RobLabels.reduce((a, b) => _storage.includeLabels.includes(b) ? a + 1 : a, 0) !== _storage.includeLabels.length) return false;
     if (map.RobLabels && map.RobLabels.filter(x => _storage.excludeLabels.includes(x)).length !== 0) return false;
     return true;
 }
@@ -84,17 +84,17 @@ function getLabels(map) {
     return map.RobLabels.map(x => m("span", { class: `map-label ${getLabelColour(x)}` }, x));
 }
 
-function includeLabel(label){
-    if(_storage.excludeLabels.includes(label)) _storage.excludeLabels = _storage.excludeLabels.filter(x => x !== label);
-    if(_storage.includeLabels.includes(label)) _storage.includeLabels = _storage.includeLabels.filter(x => x !== label);
+function includeLabel(label) {
+    if (_storage.excludeLabels.includes(label)) _storage.excludeLabels = _storage.excludeLabels.filter(x => x !== label);
+    if (_storage.includeLabels.includes(label)) _storage.includeLabels = _storage.includeLabels.filter(x => x !== label);
     else _storage.includeLabels.push(label);
 
     _storage.save();
 }
 
-function excludeLabel(label){
-    if(_storage.includeLabels.includes(label)) _storage.includeLabels = _storage.includeLabels.filter(x => x !== label);
-    if(_storage.excludeLabels.includes(label)) _storage.excludeLabels = _storage.excludeLabels.filter(x => x !== label);
+function excludeLabel(label) {
+    if (_storage.includeLabels.includes(label)) _storage.includeLabels = _storage.includeLabels.filter(x => x !== label);
+    if (_storage.excludeLabels.includes(label)) _storage.excludeLabels = _storage.excludeLabels.filter(x => x !== label);
     else _storage.excludeLabels.push(label);
 
     _storage.save();
@@ -103,11 +103,11 @@ function excludeLabel(label){
 //Get the list of tag elements for the include/exlude tag filters. Include param determines what the onclick listener does.
 function getLabelFilterList(include) {
     var getColorClass = (label) => {
-        if(include && _storage.includeLabels.includes(label)) return getLabelColour(label);
-        if(!include && _storage.excludeLabels.includes(label)) return getLabelColour(label);
+        if (include && _storage.includeLabels.includes(label)) return getLabelColour(label);
+        if (!include && _storage.excludeLabels.includes(label)) return getLabelColour(label);
         return '';
     }
-    return _foundLabels.map(x => m("span", { class: `a-self-center map-label ${getColorClass(x)}`, onclick: include ? ()=>includeLabel(x) : ()=>excludeLabel(x)}, x) );
+    return _foundLabels.map(x => m("span", { class: `a-self-center map-label ${getColorClass(x)}`, onclick: include ? () => includeLabel(x) : () => excludeLabel(x) }, x));
 }
 
 function resetFilter() {
@@ -125,106 +125,106 @@ function getRandomMap(e) {
 }
 
 function makeRow(map) {
-    return m('tr', {key: map.Name}, [
+    return m('tr', { key: map.Name }, [
         m("th", { scope: "row" }, m("a", { class: "link-secondary", href: map.Link }, map.Name)),
-        m("td", formatDate(new Date(map.InitialRatingTimestamp*1000))),
+        m("td", formatDate(new Date(map.InitialRatingTimestamp * 1000))),
         m("td", map.RobRating == null ? "Unrated" : map.RobRating),
         m("td", getLabels(map))
     ]);
 }
 
 var Filtering = {
-    view: function() {
-        return m("div", { style:{"display":"flex","justify-content":"space-between","gap":"1rem"}},
+    view: function () {
+        return m("div", { style: { "display": "flex", "justify-content": "space-between", "gap": "1rem" } },
             [
-                m("div", { style:{"flex-grow":"1"}},
-                        m("label", {class:"form-label",id:"ratingSliderText"}, "Minimum Rating: ", _storage.minRating < 0 ? 'None selected' : _storage.minRating),
-                        m("input", {
-                            class:"form-range",
-                            type:"range",
-                            min:"-0.5",
-                            max:"5",
-                            step:"0.5",
-                            value: _storage.minRating,
-                            id:"ratingSlider",
-                            oninput: (event) => {_storage.minRating = event.target.value; _storage.save();}
-                        })
+                m("div", { style: { "flex-grow": "1" } },
+                    m("label", { class: "form-label", id: "ratingSliderText" }, "Minimum Rating: ", _storage.minRating < 0 ? 'None selected' : _storage.minRating),
+                    m("input", {
+                        class: "form-range",
+                        type: "range",
+                        min: "-0.5",
+                        max: "5",
+                        step: "0.5",
+                        value: _storage.minRating,
+                        id: "ratingSlider",
+                        oninput: (event) => { _storage.minRating = event.target.value; _storage.save(); }
+                    })
                 ),
-                m("div", {style:{"flex-grow":"1"}},
-                        m("label", {class:"form-label"}, "Name filter"),
-                        m("input", {
-                                class:"form-control",
-                                type: "text",
-                                value: _storage.nameFilter,
-                                id: "nameFilter",
-                                oninput: (event) => {_storage.nameFilter = event.target.value; _storage.save();}
-                            }
-                        )
+                m("div", { style: { "flex-grow": "1" } },
+                    m("label", { class: "form-label" }, "Name filter"),
+                    m("input", {
+                        class: "form-control",
+                        type: "text",
+                        value: _storage.nameFilter,
+                        id: "nameFilter",
+                        oninput: (event) => { _storage.nameFilter = event.target.value; _storage.save(); }
+                    }
+                    )
                 ),
-                m("div", { style:{"flex-grow":"1"}},
-                        m("label", {class:"form-label"}, "Sort By"),
-                        m("div", {class:"form-check form-check-inline"},
-                            m("input", {
-                                    class:"form-check-input", 
-                                    type:"radio", 
-                                    name:"ascDescRadio", 
-                                    id:"sortAscending", 
-                                    checked: _storage.ascending,
-                                    oninput: (event) => {_storage.ascending = event.target.checked; _storage.save();}
-                                }
-                            ),
-                            m("label", {class:"form-check-label",for:"sortAscending"}, " Asc ")
+                m("div", { style: { "flex-grow": "1" } },
+                    m("label", { class: "form-label" }, "Sort By"),
+                    m("div", { class: "form-check form-check-inline" },
+                        m("input", {
+                            class: "form-check-input",
+                            type: "radio",
+                            name: "ascDescRadio",
+                            id: "sortAscending",
+                            checked: _storage.ascending,
+                            oninput: (event) => { _storage.ascending = event.target.checked; _storage.save(); }
+                        }
                         ),
-                        m("div", { class:"form-check form-check-inline"},
-                            m("input", {
-                                    class:"form-check-input",
-                                    type:"radio",
-                                    name:"ascDescRadio", 
-                                    id:"sortDescending", 
-                                    checked: !_storage.ascending,
-                                    oninput: (event) => {_storage.ascending = !event.target.checked; _storage.save();}
-                                }
-                            ),
-                            m("label", {class:"form-check-label",for:"sortDescending"}, " Desc ")
+                        m("label", { class: "form-check-label", for: "sortAscending" }, " Asc ")
+                    ),
+                    m("div", { class: "form-check form-check-inline" },
+                        m("input", {
+                            class: "form-check-input",
+                            type: "radio",
+                            name: "ascDescRadio",
+                            id: "sortDescending",
+                            checked: !_storage.ascending,
+                            oninput: (event) => { _storage.ascending = !event.target.checked; _storage.save(); }
+                        }
                         ),
-                        m("select", 
-                            {
-                                class:"form-select",
-                                id:"sortBy", 
-                                "aria-label":"Sort by a property",
-                                oninput: (event) => {_storage.sortBy = event.target.value; _storage.save();},
-                                value: _storage.sortBy
+                        m("label", { class: "form-check-label", for: "sortDescending" }, " Desc ")
+                    ),
+                    m("select",
+                        {
+                            class: "form-select",
+                            id: "sortBy",
+                            "aria-label": "Sort by a property",
+                            oninput: (event) => { _storage.sortBy = event.target.value; _storage.save(); },
+                            value: _storage.sortBy
 
-                            },
-                            _sortByProperties.map(x => m("option", {value: x.propertyName}, x.friendlyName))
-                        )
+                        },
+                        _sortByProperties.map(x => m("option", { value: x.propertyName }, x.friendlyName))
+                    )
                 )
             ]
-      );
+        );
     }
 }
 
 var Buttons = {
-    view: function() {
-        return m("div", {class:"container d-flex mt-2",style:{"justify-content":"center"}},
-          m("div", {style:{"display":"inline-block","margin":"0 4px 0 4px"}}, 
-            m("button", {class:"btn btn-primary",type:"submit",id:"resetFilterButton", onclick: resetFilter}, "Reset filters")
-          ),
-          m("div", {class:"ml-2",style:{"display":"inline-block","margin":"0 4px 0 4px"}}, 
-            m("button", {class:"btn btn-primary",type:"submit",id:"getRandomMapButton"}, "Get Random")
-          ),
-          m("div", {class:"ml-2",style:{"display":"inline-block","margin":"0 4px 0 4px"}}, 
-            m("a", {class:"btn btn-info",href:"/stats.html"}, "View statistics")
-          )
-      );
+    view: function () {
+        return m("div", { class: "container d-flex mt-2", style: { "justify-content": "center" } },
+            m("div", { style: { "display": "inline-block", "margin": "0 4px 0 4px" } },
+                m("button", { class: "btn btn-primary", type: "submit", id: "resetFilterButton", onclick: resetFilter }, "Reset filters")
+            ),
+            m("div", { class: "ml-2", style: { "display": "inline-block", "margin": "0 4px 0 4px" } },
+                m("button", { class: "btn btn-primary", type: "submit", id: "getRandomMapButton" }, "Get Random")
+            ),
+            m("div", { class: "ml-2", style: { "display": "inline-block", "margin": "0 4px 0 4px" } },
+                m("a", { class: "btn btn-info", href: "/stats.html" }, "View statistics")
+            )
+        );
     }
 }
 
 var TagFiltering = {
-    view: function() {
+    view: function () {
         return [
-            m('div', { class: 'container d-flex justify-content-around flex-wrap b-bottom mb-2 pt-2 pb-2'}, [m('h5', {class: 'mr-2'}, 'Include Labels'), ...getLabelFilterList(true)]),
-            m('div', { class: 'container d-flex justify-content-around flex-wrap b-bottom mb-2 pb-2'}, [m('h5', {class: 'mr-2'}, 'Exclude Labels'), ...getLabelFilterList(false)])
+            m('div', { class: 'container d-flex justify-content-around flex-wrap b-bottom mb-2 pt-2 pb-2' }, [m('h5', { class: 'mr-2' }, 'Include Labels'), ...getLabelFilterList(true)]),
+            m('div', { class: 'container d-flex justify-content-around flex-wrap b-bottom mb-2 pb-2' }, [m('h5', { class: 'mr-2' }, 'Exclude Labels'), ...getLabelFilterList(false)])
         ]
     }
 }
@@ -249,16 +249,15 @@ var Table = {
     }
 }
 
-function DrawRatingGraph(graphElement)
-{
+function DrawRatingGraph(graphElement) {
     var data = google.visualization.arrayToDataTable(
-        [['Date', 'Total Rated']].concat(_scrapeData.MapRatingGraphData.map(x => [new Date(x[0]*1000), x[1]]))
+        [['Date', 'Total Rated']].concat(_scrapeData.MapRatingGraphData.map(x => [new Date(x[0] * 1000), x[1]]))
     );
 
     var options = {
-    title: 'Map rating progress over time',
-    curveType: 'function',
-    legend: { position: 'bottom' }
+        title: 'Map rating progress over time',
+        curveType: 'function',
+        legend: { position: 'bottom' }
     };
 
     var chart = new google.visualization.LineChart(graphElement);
@@ -267,7 +266,7 @@ function DrawRatingGraph(graphElement)
 }
 
 var DynamicContent = {
-    view: function(){
+    view: function () {
         return [
             m(Filtering),
             m(Buttons),
@@ -277,21 +276,20 @@ var DynamicContent = {
     }
 }
 
-function findAllLabels(data){
+function findAllLabels(data) {
     _foundLabels = [];
     data.forEach(x => {
-        if(!x.RobLabels) return;
-        x.RobLabels.forEach(label => {if(!_foundLabels.includes(label)) _foundLabels.push(label)});
+        if (!x.RobLabels) return;
+        x.RobLabels.forEach(label => { if (!_foundLabels.includes(label)) _foundLabels.push(label) });
     });
     console.log(_foundLabels);
 }
 
-function formatDate(date)
-{
-    return `${_dayPrettyPrint[date.getDay()]} ${date.getDate()} ${_monthPrettyPrint[date.getMonth()]}, ${date.getYear()-100}`
+function formatDate(date) {
+    return `${_dayPrettyPrint[date.getDay()]} ${date.getDate()} ${_monthPrettyPrint[date.getMonth()]}, ${date.getYear() - 100}`
 }
 
-async function initialise(){
+async function initialise() {
     _storage.loadFromLocalStorage();
 
     m.mount($('#dynamic-content'), DynamicContent);
